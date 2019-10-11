1f9154f395ec06643c99002e9e04b854d0898038
gajendra pandeya <developerpandeya@gmail.com>
2019-10-11 07:39:16 +0545

    new commit

---
+++ app/src/main/java/com/codermonkeys/bucketdrops/Adapter/AdapterDrops.java
@@ -0,0 +1,72 @@
+package com.codermonkeys.bucketdrops.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.codermonkeys.bucketdrops.R;
+import com.codermonkeys.bucketdrops.beans.Drop;
+
+import java.util.ArrayList;
+
+import io.realm.RealmResults;
+
+public class AdapterDrops extends RecyclerView.Adapter<AdapterDrops.DropHolder> {
+    
+    private LayoutInflater mInflater;
+    
+    private RealmResults<Drop> mResults;
+
+    public AdapterDrops(Context context, RealmResults<Drop> results) {
+
+        
+        mInflater = LayoutInflater.from(context);
+        mResults = results;
+    }
+
+    private static ArrayList<String> getDummyItems() {
+
+        ArrayList<String > dummyValues = new ArrayList<>();
+        for(int i =0 ; i < 40; i++) {
+
+            dummyValues.add("Item"+ i);
+        }
+        return dummyValues;
+    }
+
+    @NonNull
+    @Override
+    public DropHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+      View view =  mInflater.inflate(R.layout.row_drop, parent, false); 
+        DropHolder holder = new DropHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DropHolder holder, int position) {
+
+
+        Drop drop = mResults.get(position);
+        holder.mTextWhat.setText(drop.getWhat());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mResults.size();
+    }
+
+    public static class DropHolder extends RecyclerView.ViewHolder {
+
+        TextView mTextWhat;
+        public DropHolder(@NonNull View itemView) {
+            super(itemView);
+            mTextWhat = itemView.findViewById(R.id.tv_what);
+        }
+    }
+}
--- app/src/main/java/com/codermonkeys/bucketdrops/MainActivity.java
+++ app/src/main/java/com/codermonkeys/bucketdrops/MainActivity.java
@@ -7,7 +7,14 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.codermonkeys.bucketdrops.Adapter.AdapterDrops;
+import com.codermonkeys.bucketdrops.beans.Drop;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,14 +22,21 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     Toolbar mToolbar;
     private Button mButtonAdd;
+    Realm mRealm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mRealm = Realm.getDefaultInstance();
+      RealmResults<Drop> results = mRealm.where(Drop.class).findAllAsync();
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mButtonAdd = (Button) findViewById(R.id.btn_add);
         mRecyclerView = (RecyclerView) findViewById(R.id.rv_drops);
+
+
+
+        mRecyclerView.setAdapter(new AdapterDrops(this, results));
         setSupportActionBar(mToolbar);
 
         mButtonAdd.setOnClickListener(new View.OnClickListener() {
